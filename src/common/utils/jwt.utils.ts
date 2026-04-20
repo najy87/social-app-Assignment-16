8c46507925f03ac826092c420edc38df4eaa196d
najy87 <najy10710@gmail.com>
2026-04-20 10:12:47 +0200

    first commit

---
+++ src/common/utils/jwt.utils.ts
@@ -0,0 +1,22 @@
+// import jwt, { Jwt, JwtPayload } from "jsonwebtoken";
+// import crypto from "node:crypto";
+
+// export function signToken(payload:JwtPayload, secret:, options:) {
+//   payload.jti = crypto.randomBytes(10).toString("hex");
+//   return jwt.sign(payload, secret, options);
+// }
+
+// export function verifyToken(token:string, secret:string) {
+//   return jwt.verify(token, secret);
+// }
+
+// export function generateTokens(payload) {
+//   const accesToken = signToken(payload, "fkdjfdjvkvsdkjvnv", {
+//     expiresIn: 60 * 200,
+//   });
+
+//   const refreshToken = signToken(payload, "fjdsgiuegegiesg", {
+//     expiresIn: "1y",
+//   });
+//   return { accesToken, refreshToken };
+// }
