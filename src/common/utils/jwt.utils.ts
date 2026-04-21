9510038b980ca4b9161320293056b8c7f8add89d
najy87 <najy10710@gmail.com>
2026-04-21 18:05:51 +0200

    Update utils

--- src/common/utils/jwt.utils.ts
+++ src/common/utils/jwt.utils.ts
@@ -1,22 +1,27 @@
-// import jwt, { Jwt, JwtPayload } from "jsonwebtoken";
-// import crypto from "node:crypto";
+import jwt, { JwtPayload, SignOptions } from "jsonwebtoken";
+import crypto from "node:crypto";
 
-// export function signToken(payload:JwtPayload, secret:, options:) {
-//   payload.jti = crypto.randomBytes(10).toString("hex");
-//   return jwt.sign(payload, secret, options);
-// }
+export function signToken(
+  payload: JwtPayload,
+  secret: string,
+  options: SignOptions,
+) {
+  payload.jti = crypto.randomBytes(10).toString("hex");
+  return jwt.sign(payload, secret, options);
+}
 
-// export function verifyToken(token:string, secret:string) {
-//   return jwt.verify(token, secret);
-// }
+export function verifyToken(token: string, secret: string) {
+  return jwt.verify(token, secret);
+}
 
-// export function generateTokens(payload) {
-//   const accesToken = signToken(payload, "fkdjfdjvkvsdkjvnv", {
-//     expiresIn: 60 * 200,
-//   });
+export function generateTokens(payload: JwtPayload) {
+  const accessToken = signToken(payload, "fkdjfdjvkvsdkjvnv", {
+    expiresIn: 60 * 200,
+  });
 
-//   const refreshToken = signToken(payload, "fjdsgiuegegiesg", {
-//     expiresIn: "1y",
-//   });
-//   return { accesToken, refreshToken };
-// }
+  const refreshToken = signToken(payload, "fjdsgiuegegiesg", {
+    expiresIn: "1y",
+  });
+
+  return { accessToken, refreshToken };
+}
